1e8f231a6b4064052d0b90110b073c4fc0e3c4ed
frojascr <fjrojas.cr@gmail.com>
2020-11-21 12:24:31 -0600

    transacciones completas
    se añadieron los mailboxes  y los constrains

--- transacciones.sv
+++ transacciones.sv
@@ -18,12 +18,13 @@ typedef enum{dtnt_aleat,dtny_espec, broadcast } tipos_dtny;
 typedef enum{pyld_aleat,pyld_espec} tipos_pyld;
 
 
-//****************************DEFINICION DE TIPOS DE MODO***********************************
-//typedef enum{modo_filas,modo_colum} tipos_modo;
+//********************************DEFINICION DE ACCION *************************************
+typedef enum{normal,reset} tipos_accion;
+
 
 
 //**************************************INTERFACE*******************************************
-interface intfz#(parameter pckg_sz, drivers,bits)(input bit clk);
+interface intfz#(parameter pckg_sz)(input bit clk);
       bit pndng[16];                                                //output
       bit [pckg_sz-1:0] data_out[16];                               //output
       bit popin[16];                                                //output
@@ -34,13 +35,14 @@ endinterface
 
 //*********************************TRANSACCION LADO DRIVER***********************************
 class Trans_in#(parameter pckg_sz=32);//transaccion del mensaje que entra al DUT
-  rand  bit [pckg_sz-1:pckg_sz-8] Nxt_jump;
-  rand  bit [pckg_sz-9:pckg_sz-16] Target; 
-  rand  bit [7:0] Origen; 
+  rand   bit [pckg_sz-1:pckg_sz-8] Nxt_jump;
+  randc  bit [pckg_sz-9:pckg_sz-16] Target; 
+  randc  bit [7:0] Origen; 
   
-  rand  bit [pckg_sz-17:pckg_sz-17] mode;
-  rand  bit [pckg_sz-18:0] payload;
-  rand  bit [2:0] delay;
+  rand   bit [pckg_sz-17:pckg_sz-17] mode;
+  rand   bit [pckg_sz-18:0] payload;
+  rand   int delay;
+  tipos_accion tipo;
 
   // Valores válidos de direcciones de los dispositivos
   bit dir={8'b00000001, 8'b00000010,8'b00000011,8'b00000100,8'b00010000,8'b00100000,8'b00110000,8'b01000000,8'b01010001,8'b01010010,8'b01010011,8'b01010100,8'b00010101,8'b00100101,8'b00110101,8'b01000101};
@@ -48,9 +50,10 @@ class Trans_in#(parameter pckg_sz=32);//transaccion del mensaje que entra al DUT
   constraint limittar {Target inside {dir};}
   constraint limitorin {Origen inside {dir};}
   constraint limitnxtjump {Nxt_jump inside {dir};}
-
+  constraint limitdly {0<=delay;delay<=3;}
+  
   function print  (string tag); // Funcion para imprimir datos
-    $display("[T=%g] %s Nxt_jump=%b, Target=%b, Modo=%b, Payload=%0d, Origen=%b, Delay= %b",
+    $display("[T=%g] %s Nxt_jump=%b, Target=%b, Modo=%b, Payload=%0d, Origen=%b, Delay= %0d",
              $time,
              tag, 
              this.Nxt_jump,
@@ -66,14 +69,14 @@ endclass
 
 //*********************************TRANSACCION LADO MONITOR**********************************
 class Trans_out#(parameter pckg_sz=32);//transaccion del mensaje que entra al DUT
-    bit [pckg_sz-1:pckg_sz-8] Nxt_jump;
-    bit [pckg_sz-9:pckg_sz-16] Target; 
-    bit [pckg_sz-17:pckg_sz-17] mode;
-    bit [pckg_sz-18:0] payload;
-    bit [2:0] delay;
+    bit [pckg_sz-1:pckg_sz-8] Nxt_jumpO;
+    bit [pckg_sz-9:pckg_sz-16] TargetO; 
+    bit [pckg_sz-17:pckg_sz-17] modeO;
+    bit [pckg_sz-18:0] payloadO;
+    int  delayO;
 
   function print  (string tag); // Funcion para imprimir datos
-    $display("[T=%g] %s Nxt_jump=%b, Target=%b, Modo=%b, Payload=%0d, Delay= %b",
+    $display("[T=%g] %s Nxt_jump=%b, Target=%b, Modo=%b, Payload=%0d, Delay= %0d",
              $time,
              tag, 
              this.Nxt_jumpO,
@@ -86,3 +89,14 @@ class Trans_out#(parameter pckg_sz=32);//transaccion del mensaje que entra al DU
   
 endclass
 
+//********************************DEFINICION DE MAILBOXES***********************************
+
+typedef mailbox #(Trans_in)mlbx_gen_agte;                  //MAILBOX GENERADOR-AGENTE
+
+typedef mailbox #(Trans_in)mlbx_agte_drv;                  //MAILBOX AGENTE-DRIVER
+
+typedef mailbox #(Trans_in)mlbx_agte_chckr;                //MAILBOX AGENTE-CHECKER
+
+typedef mailbox #(Trans_out)mlbx_mntr_chckr;               //MAILBOX MONITOR-CHECKER
+
+//*******************************************************************************************
