efab9e3d890f6de8fac55eb2b9ab8a4947b3d9a1
AgustinDS <ahusjads@gmail.com>
2020-11-21 21:37:55 -0600

    Add fork to monitor

--- monitor.sv
+++ monitor.sv
@@ -28,7 +28,7 @@ class read_dvc #(parameter pckg_sz=32);
 					to_chckr.tipo = normal;
 				end
 				to_chckr_mlbx.put(to_chckr);
-				to_chckr.print("[Monitor] Tansacción leída")
+				to_chckr.print("[Monitor] Tansacción leída");
 			end
 			@(posedge vif.clk);
 		end
@@ -36,18 +36,21 @@ class read_dvc #(parameter pckg_sz=32);
 
 endclass
 
-// 
+// Falta el fork!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 class monitor #(parameter pckg_sz=32);
 	virtual intfz #(.pckg_sz(pckg_sz)) vif; // Interfaz virtual
 	mlbx_mntr_chckr to_chckr_mlbx_p [15:0]; // Mailboxes para cada dispositivo
 	read_dvc #(.pckg_sz(pckg_sz)) dvcs [15:0];
 
 	task run();
-		for (int i = 0; i < 16; i++) begin : _n
-			dvcs[i].tag = i;
-			dvcs[i].to_chckr_mlbx = to_chckr_mlbx_p;
-			dvcs[i].vif = vif;
-			dvcs[i].run();
+		for (int i = 0; i < 16; i++) begin
+			automatic int auto_i = i;
+			fork
+				dvcs[auto_i].tag = auto_i;
+				dvcs[auto_i].to_chckr_mlbx = to_chckr_mlbx_p;
+				dvcs[auto_i].vif = vif;
+				dvcs[auto_i].run();
+			join_none
 		end		
 	endtask
 
