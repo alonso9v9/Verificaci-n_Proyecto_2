a019b7bf1c1e923e723f860a6bc7ad1b39f68fb1
Luis Alonso Vega <alonso9v9@gmail.com>
2020-11-23 10:28:11 -0600

    Update generador de parámetros

--- rand.sv
+++ rand.sv
@@ -11,10 +11,10 @@
 module random;
   
   class Params;
-    int unsigned ROWS = 4; 
-    int unsigned COLUMS =4; 
-    rand int unsigned pckg_sz;
-    rand int unsigned fifo_depth;
+    bit [2:0]ROWS = 3'd4; 
+    bit [2:0]COLUMS = 3'd4; 
+    rand int pckg_sz;
+    rand int fifo_depth;
     bit [7:0]bdcst= 8'b11111111;
 
     constraint pckg {pckg_sz<100;pckg_sz>0;}
@@ -22,7 +22,9 @@ module random;
     
 
     function void printPackage;
-        int f = $fopen("Rand_Parameters.sv");
+        int f = $fopen("./Rand_Parameters.sv","w");
+        if(f) $display("[%0g] Archivo abierto",f);
+        else $display("No se pudo abrir el archivo");
         $fdisplay(f, "package Rand_Parameters;");
         $fdisplay(f, "  parameter ROWS = %0d;",ROWS);
         $fdisplay(f, "  parameter COLUMS = %0d;",COLUMS);
@@ -30,7 +32,9 @@ module random;
         $fdisplay(f, "  parameter fifo_depth = %0d;",fifo_depth);
         $fdisplay(f, "  parameter bdcst = %b;",bdcst);
         $fdisplay(f, "endpackage");
-    endfunction
+	      $fclose(f);
+	      $display("[%0g] Archivo creado",f);
+    endfunction 
 
   endclass
 
@@ -39,7 +43,7 @@ module random;
   initial begin
     a = new();
     a.srandom(10);
-    a.randomize();
+    void'(a.randomize());
     a.printPackage();
   end
 
