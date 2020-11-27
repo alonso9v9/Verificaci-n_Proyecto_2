f8a964f81e32727d054d1e2a636764a99228b50f
AgustinDS <ahusjads@gmail.com>
2020-11-27 12:41:34 -0600

    Update comando.sh

--- comando.sh
+++ comando.sh
@@ -1,11 +1,11 @@
 source synopsys_tools.sh;
 
-rm -rfv `ls |grep -v ".*\.sv\|.*\.sh"`;
+rm -rfv `ls |grep -v ".*\.sv\|.*\.sh\|.*\.md"`;
 
 #vcs -Mupdate Test_Power.sv  -o salida -full64 -debug_all -sverilog -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
 #vcs -Mupdate Test_fifo.sv  -o salida -full64 -debug_all -sverilog -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
 #vcs -Mupdate prueba_modulo_driver.sv  -o salida -full64 -debug_all -sverilog -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
-vcs -Mupdate testbench.sv  -o salida -full64 -debug_all -sverilog -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
+vcs -Mupdate ../testbench.sv  -o salida -full64 -debug_all -sverilog -l log_test +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
 
 ./salida -cm line+tgl+cond+fsm+branch+assert;
 #dve -full64 -covdir salida.vdb &
