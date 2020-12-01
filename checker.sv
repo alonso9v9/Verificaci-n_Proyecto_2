edffee09483fa4edcfc9bd047127ca9095729030
AgustinDS <ahusjads@gmail.com>
2020-12-01 13:37:14 -0600

    Avance checker, falta instanciar y conectar routers

--- checker.sv
+++ checker.sv
@@ -1,6 +1,9 @@
 
 // Interfaz del bus
-module  bus_if_emu #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, parameter id_r =0, parameter id_c = 0, parameter columns=4, parameter rows=4) (
+// Broadcast ??????????????????????????????????????????
+// ????????????????????????????????????????????????????
+// ????????????????????????????????????????????????????
+module  bus_if_emu #(parameter pckg_sz = 40, parameter Fif_Size=10, parameter [7:0] id = 0, parameter id_r =0, parameter id_c = 0, parameter columns=4, parameter rows=4) (
 	input clk,    // Clock
 	input rst,  // Asynchronous reset active high
 	
@@ -64,7 +67,7 @@ endmodule
 
 
 // Árbitro del router
-module arbiter #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, parameter id_r =0, parameter id_c = 0, parameter columns=4, parameter rows=4) (
+module arbiter #(parameter pckg_sz = 40, parameter Fif_Size=10, parameter id_r =0, parameter id_c = 0, parameter columns=4, parameter rows=4) (
 	input clk,    // Clock
 	input rst,	  // Asynchronous reset active high
 
@@ -86,7 +89,7 @@ module arbiter #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, para
 	assign clk_en = pndng_i[0] | pndng_i[1] | pndng_i[2] | pndng_i[3];
 
 	always @(posedge clk & clk_en) begin 
-		
+		Trn = cont;
 		case (estado)
 			0:begin // rst
 				// Señales
@@ -117,6 +120,7 @@ module arbiter #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, para
 		endcase
 		estado = estado_sig;
 		Data_in_i <= Data_out_i[cont];
+		// Contador para el siguiente ciclo
 		if (Cnt_en) cont++;
 		if (cont == 4) cont = 0;
 
@@ -124,9 +128,73 @@ module arbiter #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, para
 endmodule
 
 // Definición del router individual
-module router #()(
+module router #(parameter pckg_sz = 40, parameter Fif_Size=10, parameter id_r =0, parameter id_c = 0, parameter columns=4, parameter rows=4)(
 	input clk,    // Clock
 	input rst,  // Asynchronous reset active high
+
+	input [pckg_sz-1:0] Data_out_i_in [4], 
+	input pndng_i_in [4], 
+	input pop [4],
+	output Popin [4],
+	output [pckg_sz-1:0] Data_out [4],
+	output pndng [4],
+);
+
+wire [pckg_sz-1:0] Data_out_i [4];
+wire pndng_i [4];
+
+wire [1:0] Trn;
+wire push_i;
+wire pop_i;
+wire [pckg_sz-1:0] Data_in_i;
+
+genvar i;
+
+generate
+	for (i = 0; i < 4; i++) begin : _rtr_
+		bus_if_emu #(.pckg_sz(pckg_sz), .Fif_Size(Fif_Size), .id(i), .id_r(id_r), .id_c(id_c), .columns(columns), .rows(rows)) intfc (
+			.clk(clk),
+			.rst(rst),
+			.Data_out_i_in(Data_out_i_in[i]), 
+			.pndng_i_in(pndng_i_in[i]), 
+			.pop(pop[i]),
+			.Popin(Popin[i]),
+			.Data_out(Data_out[i]),
+			.pndng(pndng[i]),
+
+			.Data_out_i(Data_out_i[i]), 
+			.pndng_i(pndng_i[i]), 
+			.pop_i(pop_i),
+			.Trn(Trn),
+			.Data_in_i(Data_in_i),
+			.push_i(push_i)
+		);
+	end
+endgenerate
+
+arbiter #(.pckg_sz(pckg_sz), .Fif_Size(Fif_Size), .id_r(id_r), .id_c(id_c), .columns(columns), .rows(rows)) rbtr_inst(
+	.clk       (clk),
+	.rst       (rst),
+	.pndng_i   (pndng_i),
+	.Data_out_i(Data_out_i),
+	.Trn       (Trn),
+	.push_i    (push_i),
+	.pop_i     (pop_i),
+	.Data_in_i (Data_in_i)
+);
+
+endmodule
+
+// Definición del emulador del mesh
+module mesh_emu #(parameter ROWS = 4, parameter COLUMS =4, parameter pckg_sz =40, parameter fifo_depth = 4, parameter bdcst= {8{1'b1}})(
+	input clk,    // Clock
+	input rst,  // Asynchronous reset active high
+	output logic pndng[ROWS*2+COLUMS*2],
+  	output [pckg_sz-1:0] data_out[ROWS*2+COLUMS*2],
+  	output logic popin[ROWS*2+COLUMS*2],
+  	input pop[ROWS*2+COLUMS*2],
+  	input [pckg_sz-1:0]data_out_i_in[ROWS*2+COLUMS*2],
+  	input pndng_i_in[ROWS*2+COLUMS*2],
 );
 
 endmodule
