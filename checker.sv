8013b3f10ed8ccc5fad9ae9c3ca31ca9d4ce7277
AgustinDS <ahusjads@gmail.com>
2020-12-01 00:50:25 -0600

    Interfaz y arbitro

--- checker.sv
+++ checker.sv
@@ -1,3 +1,5 @@
+
+// Interfaz del bus
 module  bus_if_emu #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, parameter id_r =0, parameter id_c = 0, parameter columns=4, parameter rows=4) (
 	input clk,    // Clock
 	input rst,  // Asynchronous reset active high
@@ -60,6 +62,8 @@ module  bus_if_emu #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, 
 
 endmodule
 
+
+// Árbitro del router
 module arbiter #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, parameter id_r =0, parameter id_c = 0, parameter columns=4, parameter rows=4) (
 	input clk,    // Clock
 	input rst,	  // Asynchronous reset active high
@@ -72,6 +76,57 @@ module arbiter #(parameter pckg_sz = 40, parameter Fif_Size=10, bit id = 0, para
 	output pop_i,
 	output [pckg_sz-1:0] Data_in_i
 
+);
+	wire clk_en; // Clock enable
+	int estado = 0; // Estado de la máquina de Mealy
+	int estado_sig;
+	int cont = 0; // Contador
+	bit Cnt_en;
+	
+	assign clk_en = pndng_i[0] | pndng_i[1] | pndng_i[2] | pndng_i[3];
+
+	always @(posedge clk & clk_en) begin 
+		
+		case (estado)
+			0:begin // rst
+				// Señales
+				Cnt_en <= 1;
+				push_i <= 0;
+				pop_i <= 0;
+				if (!pndng_i[cont]) begin
+					estado_sig = estado; // Estado siguiente: rst
+				end else begin
+					estado_sig = 1; 	//Estado siguiente: psh1
+				end
+			end
+			1:begin // psh1
+				estado_sig = 3; // Estado siguiente: pop1
+				// Señales
+				Cnt_en <= 0;
+				push_i <= 1;
+				pop_i <= 0;
+			end
+			3:begin // pop1
+				estado_sig = 0; // Estado siguiente: rst
+				// Señales
+				Cnt_en <= 0;
+				push_i <= 0;
+				pop_i <= 1;
+			end
+			default : $display("[Emulador: arbiter] Estado inválido");
+		endcase
+		estado = estado_sig;
+		Data_in_i <= Data_out_i[cont];
+		if (Cnt_en) cont++;
+		if (cont == 4) cont = 0;
+
+	end
+endmodule
+
+// Definición del router individual
+module router #()(
+	input clk,    // Clock
+	input rst,  // Asynchronous reset active high
 );
 
 endmodule
